dc2409318384a82fd07f98bdbe21f3f139d92f3a
Karthik <karthik.s2022cce@sece.ac.in>
2025-02-03 09:27:33 +0530

    feb-3

---
+++ Array/LC-1305.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int inc = 1, dec = 1, c1 = 1, c2 = 1;
+        for ( int i = 0; i < nums.size()-1; i++ ) {
+            if ( nums[i+1] > nums[i] ) { 
+                c1 += 1;
+                inc = max ( inc, c1 );
+                c2 = 1;
+            } else if ( nums[i+1] < nums[i] ) {
+                c2 += 1;
+                dec = max ( dec, c2 );
+                c1 = 1;
+            } else {
+                c1 = 1;
+                c2 = 1;
+            }
+        }
+        return max ( inc, dec );
+    }
+};
