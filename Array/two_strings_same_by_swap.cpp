de0edbf02f12a4788ff38a73867d098964549489
Karthik <karthik.s2022cce@sece.ac.in>
2025-02-05 20:23:34 +0530

    feb-5

---
+++ Array/two_strings_same_by_swap.cpp
@@ -0,0 +1,19 @@
+// LC - 1790
+
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if ( s1 == s2 ) return true;
+        int c = 0;
+        vector<int> temp;
+        for ( int i = 0; i < s1.size(); i++ ) {
+            if ( s1[i] != s2[i] ) {
+                c += 1;
+                temp.push_back(i);
+            }
+        }
+        if ( c != 0 && c != 2 ) return false;
+        if ( s1[temp[0]] != s2[temp[1]] || s1[temp[1]] != s2[temp[0]] ) return false;
+        return true;
+    }
+};
